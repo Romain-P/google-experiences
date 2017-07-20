f0bc3a5cb259bbc391cb50ce830514df186715c2
Romain-p <romain.pillot@epitech.eu>
2017-07-20 17:59:41 +0200

    Add class annotated analyzing ft guava.classpath

---
+++ src/com/test/classpath/sub/TestController.java
@@ -0,0 +1,12 @@
+package com.test.classpath.sub;
+
+import com.test.classpath.Controller;
+
+/**
+ * @Author: romain.pillot
+ * @Date: 20/07/2017
+ */
+@Controller("test")
+class TestController {
+
+}
